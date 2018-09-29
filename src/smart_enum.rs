7911dfbedf9013b26b8dedbc888e0e571755eb99
uraf <piotrekmis@gmail.com>
2018-09-29 14:47:21 +0200

    Concretization rework

--- src/smart_enum.rs
+++ src/smart_enum.rs
@@ -5,3 +5,32 @@ pub trait SmartEnum: Clone {
 
     fn as_usize(&self) -> usize;
 }
+
+impl <A: SmartEnum, B: SmartEnum> SmartEnum for (A, B) {
+    const LENGTH: usize = A::LENGTH * B::LENGTH;
+
+    fn values() -> &'static [Self] {
+        unimplemented!("Please implement.") // TODO[typesafe]: do it.
+    }
+
+    fn as_usize(&self) -> usize {
+        match self {
+            (a, b) => B::LENGTH * a.as_usize() + b.as_usize()
+        }
+    }
+}
+
+impl <A: SmartEnum> SmartEnum for Option<A> {
+    const LENGTH: usize = 1 + A::LENGTH;
+
+    fn values() -> &'static [Self] {
+        unimplemented!("Please implement.") // TODO[typesafe]: do it.
+    }
+
+    fn as_usize(&self) -> usize {
+        match self {
+            None => 0,
+            Some(x) => 1 + x.as_usize(),
+        }
+    }
+}
