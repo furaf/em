9319664aaa874027954282d2ca9ab881dae288d1
fulara <ntszar@gmail.com>
2018-10-24 03:02:07 +0200

    Derive is now being reexported, need to do some test on this.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
-#[cfg(test)]
 #[macro_use]
+#[allow(unused_imports)]
 extern crate smart_enum_derive;
+#[doc(hidden)]
+pub use self::smart_enum_derive::*;
 
 extern crate itertools;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+
 
 mod enum_map;
 mod smart_enum;
